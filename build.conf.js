9454cf1ed1daca2cb3a1fe1b3d88541be23a24c6
Ross Pfahler <ross.pfahler@gmail.com>
2014-09-24 09:39:33 -0700

    Revert "Add MIT license"
    This reverts commit 0f0c40a798d1397f15c46f622fb192f177732d2d.

--- build.conf.js
+++ build.conf.js
@@ -10,6 +10,12 @@
     },
     include: ['almond'],
     cjsTranslate: true,
+    uglify2: {
+      compress: {
+        unsafe: true
+      },
+      mangle: true
+    },
     wrap: {
       startFile: './tools/wrap-start.frag',
       endFile: './tools/wrap-end.frag'
